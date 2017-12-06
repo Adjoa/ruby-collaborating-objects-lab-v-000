557e30270790b7b97b61794f5a9c935b2e94da86
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-06 20:41:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Song
 
   attr_accessor :name, :artist
@@ -7,17 +6,6 @@ class Song
     @name = name
   end
 
-#   def self.new_by_filename(file_name)
-#     song = file_name.split(" - ")[1]
-#     artist_name = file_name.split(" - ")[0]
-#
-#     song_new = self.new(song)
-#     song_new.artist= artist_name
-#     song_new
-#   end
-#
-# end
-
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
@@ -25,17 +13,5 @@ class Song
     new_song.artist.add_song(new_song)
     new_song
   end
-
-# def artist_name=(name)
-#   self.artist = Artist.find_or_create_by_name(name)
-#   artist.add_song(self)
-# end
-
-  # def artist=(artist_name)
-  #   binding.pry
-  #   @artist = Artist.find_or_create_by_name(artist_name)
-  #
-  #   # artist.add_song(self)
-  # end
 
 end
